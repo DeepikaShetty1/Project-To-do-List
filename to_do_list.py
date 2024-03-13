a773d122dc40198534d12e8836444f92ffe36a1e
DeepikaShetty1 <151734934+DeepikaShetty1@users.noreply.github.com>
2024-03-13 10:26:20 +0530

    Update to_do_list.py

--- to_do_list.py
+++ to_do_list.py
@@ -1,1 +1,30 @@
+def to_do_list():
+    data = []
+    print('''Menu:
+1.Add Task
+2.Mark Task as Done
+3.View your Pending Tasks
+4.Exit''')
+    user_input = input("Enter Your Choice : ")
+    while(user_input != 4)
+        if(user_input == 1):
+            task = input("What task you have to complete?")
+            data.append(task)
+            print("Your Task Added Successfully into ToDo List")
+        elif(user_input == 2):
+            completed = input("What task you are Completed?")
+            if completed in data:
+                data.remove(completed)
+                print("Your completed task is marked as done")
+                print("Your Remaining Tasks are ",data)
+            else:
+                print("Provided Task is not in Your To-Do List")
+        elif(user_input == 3):
+            print("Your Remaining Tasks are ",data)
+    print("GoodBye! Have a Nice Day!")
+        
+
+
+
+
 
